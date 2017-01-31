7608c48ce480573e8327fe38c567d317368d3e3f
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2017-01-31 21:20:37 +0000

    Changed the layout and the API

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ weatherApp.config(function ($routeProvider) {
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
+
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
     
 });
 
@@ -33,20 +38,33 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', '$filter', function($scope, $resource, cityService, $filter) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$filter','$routeParams','cityService', function($scope, $resource, $filter, $routeParams,cityService) {
     $scope.city = cityService.city;
+    $scope.days = $routeParams.days || 2;
     
-    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast?appid=a46a814baecd22a559e0bcdff847dec9', {callback: 'JSON_CALLBACK'}, {get: { method: 'JSONP'}});
+    //Forcast API
+    // $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast?appid=a46a814baecd22a559e0bcdff847dec9', {callback: 'JSON_CALLBACK'}, {get: { method: 'JSONP'}});
 
-    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 2});
+    //Test API
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?appid=a46a814baecd22a559e0bcdff847dec9', {callback: 'JSON_CALLBACK'}, {get: { method: 'JSONP'}});
+
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days});
     
     $scope.convertToFahrenheit = function(degK){
         return Math.round((1.8 * (degK - 273)) + 32 );
     }
 
+    $scope.convertToCelcius = function(degK){
+        return Math.round(degK - 273);
+    }
+
+    // $scope.conversion = function(){
+        
+    // }
+
     $scope.convertToDate = function(dt){
         // return new Date(dt * 1000).toLocaleString();
-        return $filter('date')(new Date(dt * 1000), 'medium', 'UTC');
+        return $filter('date')(new Date(dt * 1000), 'mediumDate', 'UTC');
     }
     
 }]);
