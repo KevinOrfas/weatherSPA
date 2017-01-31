a950c29ddb985d02abed064b5fecf892b424e520
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2017-01-31 18:46:56 +0000

    Function to format date and convert deg to Fahrenheit

--- app.js
+++ app.js
@@ -46,5 +46,9 @@ weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService
     $scope.convertToFahrenheit = function(degK){
         return Math.round((1.8 * (degK - 273)) + 32 );
     }
+
+    $scope.convertToDate = function(dt){
+        return new Date(dt * 1000);
+    }
     
 }]);
